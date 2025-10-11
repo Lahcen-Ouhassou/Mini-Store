94799a98eebadfbac85b057e512cf52d179e6121
lahceenn <lahcenouhassou18@gmail.com>
2025-10-11 23:34:21 +0100

    New update

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const {
-  createProduct,
-  getProducts,
-  deleteProduct,
-} = require("../controllers/productController");
+const {createProduct,getProducts,deleteProduct,} = require("../controllers/productController");
 
 router.post("/", createProduct);
 router.get("/", getProducts);
