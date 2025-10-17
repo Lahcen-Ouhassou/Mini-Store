91778811a8d462ad43b06a954e12173fadd73618
lahceenn <lahcenouhassou18@gmail.com>
2025-10-17 22:32:22 +0100

    UPD

--- middleware/adminMiddleware.js
+++ middleware/adminMiddleware.js
@@ -1,3 +1,6 @@
+// هاد middleware هو اللي كيتأكد واش المستخدم عندو role = admin
+// باش يخليه يدير عمليات خاصة (مثلاً create product أو delete)
+
 const admin = (req, res, next) => {
   if (req.user && req.user.role === "admin") {
     next(); // ✅ عندو صلاحية admin
