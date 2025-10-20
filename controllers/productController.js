3245d98d0c51f897ebdf55c4a78154b59f815002
lahceenn <lahcenouhassou18@gmail.com>
2025-10-20 22:36:11 +0100

    UPD

--- controllers/productController.js
+++ controllers/productController.js
@@ -51,7 +51,7 @@ const getProductById = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
     if (!product)
-      return res.status(404).json({ message: "❌ Product not found" });
+      return res.status(404).json({ message: "❌ Product not Found" });
 
     res.status(200).json(product);
   } catch (error) {
