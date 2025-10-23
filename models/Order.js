7e8df1996120352722c3d00972ee584fbcb879b9
lahceenn <lahcenouhassou18@gmail.com>
2025-10-23 21:19:44 +0100

    Upd

--- models/Order.js
+++ models/Order.js
@@ -2,34 +2,15 @@ const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema(
   {
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User", // الإشارة إلى المستخدم اللي دار الطلب
-      required: true,
-    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     products: [
       {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "Product",
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          required: true,
-          min: 1,
-        },
+        product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+        quantity: { type: Number, required: true },
       },
     ],
-    totalPrice: {
-      type: Number,
-      required: true,
-    },
-    status: {
-      type: String,
-      enum: ["pending", "processing", "shipped", "delivered", "cancelled"],
-      default: "pending",
-    },
+    totalPrice: { type: Number },
+    status: { type: String, default: "pending" },
   },
   { timestamps: true }
 );
