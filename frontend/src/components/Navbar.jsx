f1557e446032292a257f0ce70c9800bfe8005cae
lahceenn <lahcenouhassou18@gmail.com>
2025-10-30 14:41:03 +0100

    New Upd Navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,11 +5,29 @@ function Navbar() {
     <nav className="bg-gray-900 text-white px-8 py-4 flex justify-between items-center shadow-md">
       {/* Logo */}
       <h1 className="text-2xl font-bold text-white">MiniStore</h1>
-
+      <div class="grid grid-cols-4 content-center gap-20">
+        <div>
+          <a href="">Home</a>
+        </div>
+        <div>
+          <a href="">Products</a>
+        </div>
+        <div>
+          <a href="">About Me</a>
+        </div>
+        <div>
+          <a href="">Contact</a>
+        </div>
+      </div>
       {/* Login Button */}
-      <button className="bg-white text-gray-900 font-semibold px-5 py-2 rounded-lg hover:bg-gray-200 transition">
-        Login
-      </button>
+      <div class="grid grid-cols-2 gap-2">
+        <button className="bg-white text-gray-900 font-semibold px-5 py-2 rounded-lg hover:bg-gray-200 transition">
+          Sign Up
+        </button>
+        <button className="bg-white text-gray-900 font-semibold px-5 py-2 rounded-lg hover:bg-gray-200 transition">
+          Login
+        </button>
+      </div>
     </nav>
   );
 }
