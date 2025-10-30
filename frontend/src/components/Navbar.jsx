47c19961432c684a980605cf3005cfdea0f5623b
lahceenn <lahcenouhassou18@gmail.com>
2025-10-31 00:11:42 +0100

    Upd

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="bg-gray-900 text-white px-8 py-4 flex justify-between items-center shadow-md">
       {/* Logo */}
-      <h1 className="text-2xl font-bold text-white">MiniStore</h1>
-      <div class="grid grid-cols-4 content-center gap-20">
-        <div>
-          <a href="">Home</a>
-        </div>
-        <div>
-          <a href="">Products</a>
-        </div>
-        <div>
-          <a href="">About Me</a>
-        </div>
-        <div>
-          <a href="">Contact</a>
-        </div>
+      <Link to="/" className="text-2xl font-bold text-white">
+        MiniStore
+      </Link>
+
+      {/* Links */}
+      <div className="grid grid-cols-4 content-center gap-20">
+        <Link to="/" className="hover:text-blue-400 transition">
+          Home
+        </Link>
+        <Link to="/products" className="hover:text-blue-400 transition">
+          Products
+        </Link>
+        <Link to="/about" className="hover:text-blue-400 transition">
+          About Me
+        </Link>
+        <Link to="/contact" className="hover:text-blue-400 transition">
+          Contact
+        </Link>
       </div>
-      {/* Login Button */}
-      <div class="grid grid-cols-2 gap-2">
+
+      {/* Login Buttons */}
+      <div className="grid grid-cols-2 gap-2">
         <button className="bg-white text-gray-900 font-semibold px-5 py-2 rounded-lg hover:bg-gray-200 transition">
           Sign Up
         </button>
