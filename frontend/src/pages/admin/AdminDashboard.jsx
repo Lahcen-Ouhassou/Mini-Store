8e847d14cdf16eb53a7bdbd39b98303421b2604f
lahceenn <lahcenouhassou18@gmail.com>
2025-11-14 18:15:40 +0100

    Upd Frontend

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -1,17 +1,36 @@
-import AdminNavbar from "../../components/admin/AdminNavbar";
-import AdminSidebar from "../../components/admin/AdminSidebar";
-import { Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import AdminSidebar from "./AdminSidebar";
+import AdminNavbar from "./AdminNavbar";
 
-export default function AdminDashboard() {
+// Pages
+import Overview from "./Overview";
+import AdminProducts from "./Products";
+import AdminOrders from "./Orders";
+import AdminUsers from "./Users";
+import AdminSettings from "./Settings";
+
+function AdminDashboard() {
   return (
     <div className="flex min-h-screen bg-gray-100">
+      {/* Sidebar */}
       <AdminSidebar />
-      <div className="flex-1 flex flex-col">
+
+      {/* Main Content */}
+      <div className="flex-1">
         <AdminNavbar />
-        <main className="p-6">
-          <Outlet />
-        </main>
+
+        <div className="p-6">
+          <Routes>
+            <Route path="overview" element={<Overview />} />
+            <Route path="products" element={<AdminProducts />} />
+            <Route path="orders" element={<AdminOrders />} />
+            <Route path="users" element={<AdminUsers />} />
+            <Route path="settings" element={<AdminSettings />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
 }
+
+export default AdminDashboard;
